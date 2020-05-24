9809730ff95e6da0d394d402b2e265d0117240c3
Luis Eduardo Alcantara Olvera <geekshinobi@gmail.com>
2020-05-24 18:28:30 -0500

    Ejemplo de prototype

---
+++ objetos_constructores_this/prototypes.js
@@ -0,0 +1,35 @@
+function Cliente(nombre, saldo){
+    this.nombre = nombre;
+    this.saldo = saldo;
+}
+// crear un prototype
+Cliente.prototype.tipoCliente = function (){
+    let tipo;
+    // usamos this
+    if (this.saldo > 1000) {
+        tipo = "Gold";
+    }else if(this.saldo > 500){
+        tipo = 'Platino'
+    }else {
+        tipo = 'Normal';
+    }
+    return tipo;
+}
+// prototipo que impreime saldo y nombre
+Cliente.prototype.nombreClienteSaldo = function (){
+    return `Nombre ${this.nombre}, tu saldo es de ${this.saldo}, Tipo de cliente: ${this.tipoCliente()}`
+}
+// prototype para retirar saldo
+Cliente.prototype.retirarSaldo = function (retiro){
+    return this.saldo -= retiro;
+}
+// instanciamos a cada objeto
+const cliente1 = new Cliente('Pedro', 100);
+const cliente2 = new Cliente('Luis', 600);
+const cliente3 = new Cliente('Ana', 1001);
+// retiramos saldo por medio de un prototype
+cliente2.retirarSaldo(300);
+// accedemos a cada protorype
+console.log(cliente1.nombreClienteSaldo());
+console.log(cliente2.nombreClienteSaldo());
+console.log(cliente3.nombreClienteSaldo());
