bf81a58d2d280757d3fc89bc8f36ee3784e57343
Luis Eduardo Alcantara Olvera <geekshinobi@gmail.com>
2020-05-27 19:55:10 -0500

    Al dar click al submit ingresmos al select de marca y optenemos el value y el texto del option seleccionado

--- cotiza_seguro_de_auto/js/app.js
+++ cotiza_seguro_de_auto/js/app.js
@@ -1,3 +1,23 @@
+// Constructor para seguro
+function Seguro(marca, anio, tipo){
+    this.marca = marca;
+    this.anio = anio;
+    this.tipo = tipo;
+}
+
+// Todo lo que se meustra
+function Interfas(){}
+
+// EventListener
+const formulario = document.getElementById('cotizar-seguro')
+formulario.addEventListener('submit', function(e){
+    e.preventDefault();
+    const marca = document.getElementById('marca');
+    const marcaSeleccionada = marca.options[marca.selectedIndex].value;
+    const marcaSeleccionadaText = marca.options[marca.selectedIndex].textContent;
+    console.log('Presionado', marcaSeleccionada, marcaSeleccionadaText)
+})
+
 // creamos el select con el listado de años
 const max = new Date().getFullYear(),
     min = max - 20;
