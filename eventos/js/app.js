28d74d774eb74cbcf535a62ae1b676465b3aa922
Luis Eduardo Alcantara Olvera <geekshinobi@gmail.com>
2020-06-06 17:59:20 -0500

    Se revisa el contenido del formulario

--- eventos/js/app.js
+++ eventos/js/app.js
@@ -1,3 +1,21 @@
 // instanciar ambas clases
 const eventbrite = new EventBrite();
 const ui = new Interfaz();
+
+// Listener al buscador
+document.getElementById('buscarBtn').addEventListener('click', (e) => {
+    e.preventDefault();
+    
+    // Leer el texto del input buscar
+    const textoBuscador = document.getElementById('evento').value;
+    // Leer el select
+    const categorias = document.getElementById('listado-categorias');
+    const categoriaSeleccionada = categorias.options[categorias.selectedIndex].value;
+
+    // revisar que hay algo escrito en el buscador
+    if(textoBuscador !== ''){
+        console.log('buscando');
+    }else{
+        console.log('no hay nada')
+    }
+})
