1d13ccfb021995c314ed9b809f89fd987c5fedd9
Luis Eduardo Alcantara Olvera <geekshinobi@gmail.com>
2020-06-08 22:20:49 -0500

    Importamos clases y sus metodos

--- moduloes-es6/js/app.js
+++ moduloes-es6/js/app.js
@@ -1,8 +1,14 @@
 // importamos
-import { nombreCliente, ahorro, mostrarInformacion} from './cliente.js';
+import { nombreCliente, ahorro, mostrarInformacion, Cliente} from './cliente.js';
 
 console.log(nombreCliente)
 console.log(ahorro)
 
 const funcion = mostrarInformacion(nombreCliente, ahorro)
 console.log(funcion);
+
+// utilizar una clase exportada
+let cliente = new Cliente(nombreCliente, ahorro);
+console.log(cliente);
+// usamos el metodo de la clase
+console.log(cliente.mostrarInformacion())
--- moduloes-es6/js/cliente.js
+++ moduloes-es6/js/cliente.js
@@ -6,3 +6,14 @@ export function mostrarInformacion(nombre, ahorro){
     return `cliente ${nombre} ahorro: ${ahorro}`;
 }
 
+
+// exportar una clase con sus metodos
+export class Cliente {
+    constructor(nombre, ahorro){
+        this.nombre = nombre;
+        this.ahorro = ahorro;
+    }
+    mostrarInformacion(){
+        return `Cliente: ${this.nombre} - Ahorro: ${this.ahorro}`;
+    }
+}
