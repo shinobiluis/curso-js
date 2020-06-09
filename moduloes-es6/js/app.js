b88ff3e27f5063fd0fbe28fe3b33b9a9edd9b8a7
Luis Eduardo Alcantara Olvera <geekshinobi@gmail.com>
2020-06-08 22:10:40 -0500

    Exportamos e importamos variable y una funcion

---
+++ moduloes-es6/js/app.js
@@ -0,0 +1,8 @@
+// importamos
+import { nombreCliente, ahorro, mostrarInformacion} from './cliente.js';
+
+console.log(nombreCliente)
+console.log(ahorro)
+
+const funcion = mostrarInformacion(nombreCliente, ahorro)
+console.log(funcion);
---
+++ moduloes-es6/js/cliente.js
@@ -0,0 +1,8 @@
+// exportamos las variables
+export const nombreCliente = "juan";
+export let ahorro = 200;
+// exportamos la funcion
+export function mostrarInformacion(nombre, ahorro){
+    return `cliente ${nombre} ahorro: ${ahorro}`;
+}
+
