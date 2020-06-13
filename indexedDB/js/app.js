7e544958e53d7ec9ea3678a008cad5c6d359a9ae
Luis Eduardo Alcantara Olvera <geekshinobi@gmail.com>
2020-06-13 17:54:34 -0500

    Creamos la base de datos en js

--- indexedDB/js/app.js
+++ indexedDB/js/app.js
@@ -10,3 +10,22 @@ const form = document.querySelector('form'),
     sintomas = document.querySelector('#sintomas'),
     citas = document.querySelector('#citas'),
     headingAdministra = document.querySelector('#administra');
+
+// Esperar por el DOM Ready
+document.addEventListener('DOMContentLoaded', () => {
+    // crear la base de datos
+    let crearDB = window.indexedDB.open('citas', 1);
+
+    // Si hay un error se envia en la consola
+    crearDB.onerror = function(){
+        console.log('hubo un error');
+    }
+
+    // si todo esta bien entonces muestra en consola, y asignar la base de datos.
+    crearDB.onsuccess = function(){
+        console.log('Todo listo!!');
+        // Asignar a la base de datos
+        DB = crearDB.result;
+        console.log(DB)
+    }
+})
